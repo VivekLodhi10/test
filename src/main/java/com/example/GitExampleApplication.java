4da7eedec90e3a022243069f99f96438b81e1759
VivekLodhi10 <100037810+VivekLodhi10@users.noreply.github.com>
2025-03-10 18:53:41 +0530

    Update GitExampleApplication.java

--- src/main/java/com/example/GitExampleApplication.java
+++ src/main/java/com/example/GitExampleApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitExampleApplication {
 
+	int x = 100;
+
 	public static void main(String[] args) {
 		SpringApplication.run(GitExampleApplication.class, args);
 	}
